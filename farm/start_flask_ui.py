a9893db6b60c9e3ce877b3ba99a034c9faee3528
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2021-11-06 19:32:58 +0300

    Create start_flask_ui.py

---
+++ farm/start_flask_ui.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+""" Starts the farm flask UI """
+import argparse
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from farm.config import ENABLE_FLASK_PROFILER
+from farm.farm_flask.app import create_app
+
+app = create_app()  # required by the wsgi config and main()
+
+# Flask profiler
+if ENABLE_FLASK_PROFILER:
+    import flask_profiler
+    flask_profiler.init_app(app)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Farm Flask HTTP server.")
+
+    options = parser.add_argument_group('Options')
+    options.add_argument('-d', '--debug', action='store_true',
+                         help="Run Flask with debug=True (Default: False)")
+    options.add_argument('-s', '--ssl', action='store_true',
+                         help="Run Flask without SSL (Default: Enabled)")
+
+    args = parser.parse_args()
+
+    debug = args.debug
+
+    if args.ssl:
+        app.run(host='0.0.0.0', port=80, debug=debug)
+    else:
+        # Locate the SSL certificates for forced-HTTPS
+        file_path = os.path.abspath(__file__)
+        dir_path = os.path.dirname(file_path)
+        cert = os.path.join(dir_path, "farm_flask/ssl_certs/server.crt")
+        privkey = os.path.join(dir_path, "farm_flask/ssl_certs/server.key")
+        context = (cert, privkey)
+        app.run(host='0.0.0.0', port=443, ssl_context=context, debug=debug)
