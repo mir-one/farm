5540b8eff6222b4ac1f2a31e5feab6db96fb9801
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2021-11-06 19:16:32 +0300

    db/py

---
+++ databases/alembic_post_utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# alembic_post_utils.py - Helper functions for alembic_post.py
+#
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(__file__, "../..")))
+
+from farm.config import ALEMBIC_UPGRADE_POST
+
+
+def read_revision_file():
+    try:
+        with open(ALEMBIC_UPGRADE_POST, 'r') as fd:
+            return fd.read().splitlines()
+    except Exception:
+        return []
+
+
+def write_revision_post_alembic(revision):
+    with open(ALEMBIC_UPGRADE_POST, 'a') as versions_file:
+        versions_file.write('{}\n'.format(revision))
